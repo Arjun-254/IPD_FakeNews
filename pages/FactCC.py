3d6f751180f9dcdb7795aabd5ebbe2d6e0f1c0fb
Hetens <92987462+Hetens@users.noreply.github.com>
2023-12-10 23:51:12 +0530

    Fact CC integrated

---
+++ pages/FactCC.py
@@ -0,0 +1,46 @@
+from transformers import pipeline
+import streamlit as st
+from scrapingbsf import make_data
+import pandas as pd
+
+st.title("FactCC Text Classification")
+
+# Create text and summary input areas
+summary_input = ""
+text_input = st.text_area("Enter the Headline:")
+
+# Check if 'scraped_df' is already in the session state, otherwise initialize it
+if 'scraped_df' not in st.session_state:
+    st.session_state.scraped_df = pd.DataFrame()
+
+def dataframegen(text_input):
+    scraped_df = make_data(text_input)
+    scraped_df.dropna(inplace=True)
+    return scraped_df
+
+if st.button('Scrape'):
+    st.session_state.scraped_df = dataframegen(text_input)
+
+if 'content' in st.session_state.scraped_df.columns:
+    summary_input = st.session_state.scraped_df.iloc[0]['Content']
+else:
+    summary_input = ""
+
+# Display the scraped_df
+st.dataframe(st.session_state.scraped_df)
+
+# Check if text and summary are provided
+if st.button("Classify"):
+    # Load the FactCC model
+    pipe = pipeline(model="manueldeprada/FactCC")
+
+    # Perform text classification
+    ans = pipe([[[text_input, summary_input]]], truncation='only_first', padding='max_length')
+
+    # Display the result
+    if ans[0]['label'] == 'INCORRECT':
+        ans[0]['score'] = 1 - ans[0]['score']   
+    st.subheader("Classification Result")
+    st.text("Correctness: " + str(ans[0]['score']))
+    st.progress(ans[0]['score'])
+    st.text("Label: " + ans[0]['label'])
