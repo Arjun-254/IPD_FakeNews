6f0092c49442d71fca16dad5d769259d149b7701
Hetens <92987462+Hetens@users.noreply.github.com>
2023-12-10 17:43:10 +0530

    scraping

--- scrapingbsf.py
+++ scrapingbsf.py
@@ -1,14 +1,50 @@
-import bs4
+from googlesearch import search
 from bs4 import BeautifulSoup
-import pandas as pd
 import requests
 
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
-link="https://in.indeed.com/jobs?q=Data+Scientist&l=Bangalore%2C+Karnataka&start=10"
-r=requests.get(link,headers=headers)
-print(r.status_code)
-soup=BeautifulSoup(r.text,"html.parser")
-print(soup)
-href_list =[]
-base_link="https://in.indeed.com"
-# find all element of tag
+def get_top_links(query, num_links=10):
+    linksgot =[]
+    try:
+        # Perform Google search and get the top links
+        search_results = search(query, num_results=num_links)
+
+        # Print the top links
+        print(f"Top {num_links} links for '{query}':")
+        for i, link in enumerate(search_results, start=1):
+            print(f"{i}. {link}")
+            linksgot.append(link)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    return linksgot
+
+# Example usage:
+query_to_search = "Sam Altman Was fired From OpenAI"
+search_query_results = get_top_links(query_to_search)
+
+# Scrape <p> and <h1> tags from the first search result
+def get_title_and_content(search_query_results):
+    article_titles =[]
+    article_content =[]
+    if search_query_results:
+        for results in search_query_results:
+            try:
+                # Send a request to the URL and get the HTML content
+                response = requests.get(results)
+                soup = BeautifulSoup(response.content, 'html.parser')
+
+                # Scrape <p> tags
+                p_tags = soup.find_all('p')
+                for p in p_tags:
+                    article_content.append(p.text)
+
+                # Scrape <h1> tags
+                h1_tags = soup.find_all('h1')
+                for h1 in h1_tags:
+                    article_titles.append(h1.text)
+            except Exception as e:
+                print(f"An error occurred: {e}")
+    return article_titles, article_content
+
+
+
+
