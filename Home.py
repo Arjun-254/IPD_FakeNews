f170878230a3c8e058fd2d1987e74f1cc21cd552
Arjun-254 <arjun.shah254@gmail.com>
2024-01-19 22:33:11 +0530

    punct download

--- Home.py
+++ Home.py
@@ -5,6 +5,7 @@ import streamlit as st
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 import time
+import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
@@ -17,6 +18,8 @@ from requests_html import HTMLSession
 import requests
 from NewsScrape import NewsScrape
 
+nltk.download('punkt')
+
 st.set_page_config(layout="wide")
 
 
@@ -25,9 +28,6 @@ st.caption("Aims to reduce misinformation")
 user_input = st.text_area('Please enter your article headline')
 
 vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
-
-
-
 
 
 def clean_words(new_tokens):
@@ -51,17 +51,17 @@ def read_clean_data():
 
 X_train, X_test, y_train, y_test = read_clean_data()
 
-X_train_vectorized = vectorizer.fit_transform(X_train)  # Fit the vectorizer once
+X_train_vectorized = vectorizer.fit_transform(
+    X_train)  # Fit the vectorizer once
 X_test_vectorized = vectorizer.transform(X_test)
 
 
 @st.cache_resource
 def load_model():
-    clf = LogisticRegression(max_iter =1000)
+    clf = LogisticRegression(max_iter=1000)
     clf.fit(X_train_vectorized, y_train)
     score = clf.score(X_test_vectorized, y_test)
     return clf
-
 
 
 if st.button('Predict'):
